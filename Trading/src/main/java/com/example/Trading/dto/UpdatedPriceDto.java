6bb211fa4188bfd4639ef418ae0872d58ee05545
manoj.katam <manoj.v@marketsimplified.com>
2023-11-03 11:54:43 +0530

    some changes and test case addded

---
+++ Trading/src/main/java/com/example/Trading/dto/UpdatedPriceDto.java
@@ -0,0 +1,11 @@
+package com.example.Trading.dto;
+
+import lombok.Data;
+
+@Data
+public class UpdatedPriceDto {
+    private String infoMsg;
+    private String stockSymbol;
+    private double price;
+
+}
