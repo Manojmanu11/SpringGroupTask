8d8deaad65012d6835337c8b90040b81f1057c2f
keerthanaDeviVenkatesan <keerthanadevi.v@marketsimplified.com>
2023-11-06 17:41:53 +0530

    updated testing in GetTradeHistory commit

--- Trading/src/main/java/com/example/Trading/dto/TradeDto.java
+++ Trading/src/main/java/com/example/Trading/dto/TradeDto.java
@@ -1,11 +1,15 @@
 package com.example.Trading.dto;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TradeDto {
 
     private String stockSymbol;
