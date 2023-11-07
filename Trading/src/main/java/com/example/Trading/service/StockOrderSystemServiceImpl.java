4cf04f002df69095f8367e30f238dd8a92e8a8b1
NITHISHMADHAVAN <85874407+NITHISHMADHAVAN@users.noreply.github.com>
2023-11-07 13:25:44 +0530

    Update StockOrderSystemServiceImpl.java
    remove repeated code

--- Trading/src/main/java/com/example/Trading/service/StockOrderSystemServiceImpl.java
+++ Trading/src/main/java/com/example/Trading/service/StockOrderSystemServiceImpl.java
@@ -7,11 +7,7 @@ import com.example.Trading.dto.UpdatedPriceDto;
 import com.example.Trading.entity.Order;
 import com.example.Trading.entity.Trade;
 
-
 import com.example.Trading.exception.InvalidOrderTypeException;
-
-import com.example.Trading.repository.OrderRepository;
-import com.example.Trading.repository.TradeRepository;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -26,12 +22,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
-
 import java.time.LocalDateTime;
-import java.util.List;
-
-import java.util.stream.Collectors;
-
 
 
 @AllArgsConstructor
@@ -56,28 +47,6 @@ public class StockOrderSystemServiceImpl implements StockOrderSystemService {
         this.orderRepository = orderRepository;
         this.tradeRepository = tradeRepository;
     }
-
-   // UpdateService for stockSymbols UpdatePrice
-    @Override
-
-    public UpdatedPriceDto updatePrice(OrderDto req) {
-        List<Order> stockOrders = orderRepository.findByStockSymbol(req.getStockSymbol());
-
-        if (!stockOrders.isEmpty()) {
-            double newPrice = req.getPrice();
-
-            for (Order order : stockOrders) {
-                order.setPrice(newPrice);
-                orderRepository.save(order);
-            }
-
-            UpdatedPriceDto priceDTO = new UpdatedPriceDto();
-            priceDTO.setInfoMsg("Successfully Updated");
-            priceDTO.setStockSymbol(req.getStockSymbol());
-            priceDTO.setPrice(newPrice);
-            return priceDTO;
-        }
-        return null; // Return null if no orders were found for the stock symbol
 
     public Order addOrder(OrderDto orderDto) {
         Order order = new Order();
