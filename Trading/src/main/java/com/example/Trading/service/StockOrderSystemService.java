dc62f947f6b3376991d5d082c115b3484d3efc59
NITHISHMADHAVAN <85874407+NITHISHMADHAVAN@users.noreply.github.com>
2023-11-07 16:37:56 +0530

    Update StockOrderSystemService.java
    removed unwanted code

--- Trading/src/main/java/com/example/Trading/service/StockOrderSystemService.java
+++ Trading/src/main/java/com/example/Trading/service/StockOrderSystemService.java
@@ -3,28 +3,17 @@ package com.example.Trading.service;
 import com.example.Trading.dto.OrderDto;
 import com.example.Trading.dto.TradeDto;
 import com.example.Trading.dto.UpdatedPriceDto;
-
-public interface StockOrderSystemService {
-
-    OrderDto addOrder(OrderDto orderDto);
-    UpdatedPriceDto updatePrice(OrderDto orderDto);
-    OrderDto matchOrders(OrderDto orderDto);
-
 import com.example.Trading.entity.Order;
 import com.example.Trading.entity.Trade;
-
-import java.util.List;
-
 import java.util.List;
 
 public interface StockOrderSystemService {
-
+    
     Order addOrder(OrderDto orderDto);
 
     UpdatedPriceDto updatePrice(OrderDto req);
 
     void matchOrder();
-
 
     OrderDto getPortfolio(OrderDto orderDto);
 
